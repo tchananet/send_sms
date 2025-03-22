1626a25ec0983082d774be3b971181e580410119
unknown <netacy2015@gmail.com>
2025-03-22 15:12:22 +0100

    Whatsapp sends also documents now, after a delay of like 2 mins though

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import receive_webhook
+from .views import receive_webhook_recall
 
 urlpatterns = [
     path("webhook/", receive_webhook, name="webhook"),
+    path("send_whatsapp_recall/", receive_webhook_recall, name="webhook"),
 ]
--- core/views.py
+++ core/views.py
@@ -29,23 +29,18 @@ WHATSAPP_API_URL = "https://app.techsoft-sms.com/api/v3/sms/send"
 CAR_MODEL_MAPPING = {
     19: "Berline MG7",
     27: "CHANGAN 4X4 UNI-K",
-    26: "CHANGAN CS55 PLUS",
-    23: "CYBERTANK",
-    43: "Denza D9 Hybrid 4X4 personnalisé",
-    42: "Dongfeng M-hero M50",
+    26: "CHANGAN CS55 PLUS", 
     13: "Geely Cool",
     18: "Haval H5",
     52: "Haval H6",
-    9: "Haval M6 Plus",
-    24: "Kit de protection",
-    38: "Pare-buffle",
+    9: "Haval M6 Plus", 
     16: "Pick-Up Rich 6",
-    21: "Tank 300 Standard",
+    21: "Tank 300",
 }
 
 
 @api_view(["POST"])
-def receive_webhook(request):
+def receive_webhook_recall(request):
     # logging.error(f"Received Webhook Data: {request.data}")
     # logging.error(f"Received Webhook Header: {request.headers}")
     """
@@ -118,7 +113,15 @@ def receive_webhook(request):
         "recipient": whatsapp_number,
         "sender_id": "237692091685",
         "type": "whatsapp",
-        "message": message_content
+        "message": message_content,
+        }
+
+        whatsapp_payload_w_document =   {  
+        "recipient": whatsapp_number,
+        "sender_id": "237692091685",
+        "type": "whatsapp",
+        "message": "FICHE TECHNIQUE ALPHA MOTORS",
+        "media_url":"https://meek-nasturtium-1b6cb0.netlify.app/fichetechnique.pdf"
         }
         # logging.error(f"Received Webhook Data: {sms_payload}")
         
@@ -147,6 +150,120 @@ def receive_webhook(request):
         logging.error(e)
         return Response({"error": str(e)}, status=500)
 
+@api_view(["POST"])
+def receive_webhook(request):
+    # logging.error(f"Received Webhook Data: {request.data}")
+    # logging.error(f"Received Webhook Header: {request.headers}")
+    """
+    Receives webhook data from Odoo and triggers an async SMS sending task.
+    """
+    
+
+    try:
+        data = request.data  # Parse JSON payload from Odoo
+        
+        # Extract required fields with fallbacks
+        title = TITLE_MAPPING.get(data.get("title"), "Cher")  # Default to "Cher Client"
+        display_name = data.get("display_name", "Client")
+        car_model = CAR_MODEL_MAPPING.get(data.get("x_studio_many2one_field_44a_1iffl5utu"), "un de nos véhicules")
+
+        # Format the personalized message
+        message_content = f"""Bonjour {title} {display_name},
+
+        C’est le service client d'Alpha Motors.
+
+        Merci encore d’avoir visité notre stand et pour l’intérêt que vous portez au véhicule {car_model}. Comme convenu, vous pouvez consulter notre catalogue complet.
+
+        Si vous avez des questions ou si vous souhaitez plus de détails, n’hésitez surtout pas à nous écrire par WhatsApp ou à nous appeler au 692 091 685 / 650 654 797. Nous serons ravis de vous assister.
+
+        Excellente journée à vous, et à bientôt chez Alpha Motors !
+        """
+
+        SMS_message_content = f"""Bonjour {title} {display_name},
+        Merci d’avoir visité notre stand et pour l’intérêt porté au {car_model}. Retrouvez notre catalogue en ligne ici : https://shr.pn/alpha-motors. 
+        Pour toute question, contactez-nous au 692 091 685 ou via WhatsApp. À bientôt chez Alpha Motors !
+        """ 
+        
+        # Assume phone number is retrieved elsewhere (e.g., another API call)
+        phone_number = str(data.get("phone", 0)).replace(" ","")
+        phone_number = format_cameroon_number(phone_number)
+
+        whatsapp_number = str(data.get("mobile", 0)).replace(" ","")
+        whatsapp_number = format_cameroon_number(whatsapp_number)
+        if "dm" in whatsapp_number:
+            whatsapp_number=phone_number
+
+        # Headers
+        headers = {
+            "Authorization": f"Bearer {settings.techsoft}",
+            "Content-Type": "application/json"
+        }
+
+        # Data payload
+        data = {
+            "recipient": "237655272036",
+            "sender_id": "237657467945",
+            "type": "whatsapp",
+            "message": "This is a test message"
+        }
+
+        if phone_number=="0" and whatsapp_number=="0" :
+            logging.error(f"{phone_number} whatsapp is {whatsapp_number}")
+            return Response({"status": "failed", "message": "No Phone Number"}, status=200)
+        # Send SMS asynchronously
+        # send_sms_task.delay(phone_number, message_content)
+        
+        """
+        Sends an SMS asynchronously using an external API.
+        """
+        sms_payload =   {
+        "sender": "AlphaMotors",
+        "recipient": phone_number,
+        "text": SMS_message_content
+        }
+
+        whatsapp_payload =   {  
+        "recipient": whatsapp_number,
+        "sender_id": "237692091685",
+        "type": "whatsapp",
+        "message": message_content
+        }
+        # logging.error(f"Received Webhook Data: {sms_payload}")
+
+        whatsapp_payload_w_document =   {  
+        "recipient": whatsapp_number,
+        "sender_id": "237692091685",
+        "type": "whatsapp",
+        "message": "FICHE TECHNIQUE ALPHA MOTORS",
+        "media_url":"https://meek-nasturtium-1b6cb0.netlify.app/fichetechnique.pdf"
+        }
+        
+        try:
+            if whatsapp_number != "False":
+                sent_whatsapp = send_Whatsapp(whatsapp_payload, headers, whatsapp_payload_w_document)   
+                if sent_whatsapp :
+                    pass
+                else:
+                    send_SMS(sms_payload)
+            else:
+                send_SMS(sms_payload)
+                # send_Whatsapp(whatsapp_payload, headers)
+            # send_Whatsapp(whatsapp_payload, headers)
+        except Exception as e:
+            logging.error(e)
+            return Response({"error": str(e)}, status=500)
+        
+        return Response({"status": "success", "message": "SMS Sent"}, status=200)
+
+
+        # return Response({"status": "success", "message": "SMS task queued"})
+
+    except Exception as e:
+        print(e)
+        logging.error(e)
+        return Response({"error": str(e)}, status=500)
+
+
 
 def format_cameroon_number(phone):
     """
@@ -172,7 +289,7 @@ def send_SMS(payload):
         logging.error(f"SMS FAILED: PAYLOAD = {payload}")
         return False
     
-def send_Whatsapp( payload, headers):
+def send_Whatsapp( payload, headers, whatsapp_payload_w_document={}):
     """
     Converts a 9-digit phone number starting with '6' into the international format (+237).
     """
@@ -180,6 +297,11 @@ def send_Whatsapp( payload, headers):
     response = requests.post(WHATSAPP_API_URL, headers=headers, json=payload) 
 
     if response.status_code == 200: 
+        next_response = requests.post(WHATSAPP_API_URL, headers=headers, json=whatsapp_payload_w_document) 
+        if next_response.status_code == 200:  
+            return True
+        else:
+            logging.error(f"NEXT WHATSAPP FAILED: PAYLOAD = {whatsapp_payload_w_document}")
         return True
     else: 
         logging.error(f"WHATSAPP FAILED: PAYLOAD = {payload}")
